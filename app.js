bfa357174118014823a3a23f95f3896a657ec268
KE-378 <syedharisqazi@gmail.com>
2024-05-28 07:38:14 +0500

    adding code

---
+++ app.js
@@ -0,0 +1,26 @@
+var assignmentarray = [
+    {
+        title: 'Text Editor
+        Link : 
+        date = "28-May-2024"
+    }
+];
+
+var assignList = document.getElementById('all assignments');
+for (var i=0 ; i < assignmentarray.length; i++){
+    var div = <div id 'all assignments'="row">
+    <div class="col-sm-6 mb-3 mb-sm-0">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+   
+      </div>
+    </div>
+  </div>
+</div>
+}
